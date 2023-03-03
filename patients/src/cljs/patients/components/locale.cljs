17db2d7438f979e0bd90722c862f7d00c96bf4f2
Sergey Sizov <harrior@gmail.com>
2023-03-03 11:31:38 +0400

    feat: added english locale

--- patients/src/cljs/patients/components/locale.cljs
+++ patients/src/cljs/patients/components/locale.cljs
@@ -9,6 +9,7 @@
         :app/save "Сохранить"
         :app/patient-data "Данные пациента"
         :app/patient-address "Адрес"
+        :app/patients-list "Список пациентов"
 
         :patient/fullname "ФИО"
         :patient/gender "Пол"
@@ -22,7 +23,31 @@
         :address/line "Адрес"
         :address/text "Адрес"
         :address/postal-code "Индекс"
-        :address/district "Район"}})
+        :address/district "Район"}
+
+   :en {:app/title "Patient database"
+        :app/add-patient "Add patient"
+        :app/edit-patient "Edit Patient"
+        :app/search-placeholder "Search"
+        :app/create-patient "Create"
+        :app/save "Save"
+        :app/patient-data "Patient data"
+        :app/patient-address "Address"
+        :app/patients-list "Patients list"
+
+        :patient/fullname "Name"
+        :patient/gender "Gender"
+        :patient/birthday "Birthday"
+        :patient/insurance-number "Insurance Number"
+        :patient/family "Last Name"
+        :patient/name "First Name"
+        :patient/patronymic "Patronymic" ;; Middle name?
+        :address/city "City"
+        :address/state "State"
+        :address/line "Address"
+        :address/text "Address"
+        :address/postal-code "Postal Code"
+        :address/district "District"}})
 
 (defn locale
   ([key]
