dded9a324bfb4c57358471a68a1b2ff78fb734f8
Sergey Sizov <harrior@gmail.com>
2023-03-05 00:22:46 +0400

    refactor: refactor grid-page

--- patients/src/cljs/patients/pages/patient_grid.cljs
+++ patients/src/cljs/patients/pages/patient_grid.cljs
@@ -1,10 +1,10 @@
 (ns patients.pages.patient-grid
-  (:require [ajax.edn :as edn]
-            [day8.re-frame.http-fx]
-            [re-frame.core :as rf]
+  (:require [re-frame.core :as rf]
+            [patients.nav :as nav]
             [patients.components.locale :refer [locale]]
             [patients.components.table :refer [table table-header]]
-            [patients.components.ui-elements :as ui]))
+            [patients.components.requests :as rpc]
+            [patients.components.ui-elements :refer [button]]))
 
 ;;
 ;; Subs
@@ -20,7 +20,7 @@
 ;;
 
 (rf/reg-event-db
- :update-patients-db
+ :load-patients-db
  (fn [db [_ params]]
    (assoc db
           :patients
@@ -30,6 +30,14 @@
                reverse))))
 
 (rf/reg-event-db
+ :create-patient
+ (fn [db]
+   (-> db
+       (assoc-in [:app :current-page] :patient-single)
+       (assoc-in [:app :patient-uid] nil))))
+
+;; NOTE: add error show
+(rf/reg-event-db
  :bad-http-result
  (fn [_ _]
    (println "Something wrong!")))
@@ -37,21 +45,17 @@
 (rf/reg-event-fx
  :request-patients-list
  (fn [_ _]
-   {:http-xhrio {:method          :post
-                 :uri             "http://localhost:8000/rpc"
-                 :params          {:method :list-patients}
-                 :timeout         8000
-                 :format          (edn/edn-request-format)
-                 :response-format (edn/edn-response-format {:keywords? true})
-                 :on-success      [:update-patients-db]
-                 :on-failure      [:bad-http-result]}}))
+   {:dispatch [::rpc/invoke
+               {:method :list-patients}
+               [:load-patients-db]
+               [:bad-http-result]]}))
 
 ;;
 ;; Patients grid page
 ;;
 
 (defn init []
-  (rf/dispatch-sync [:request-patients-list]))
+  (rf/dispatch [:request-patients-list]))
 
 (defn patient-grid-page
   []
@@ -61,15 +65,16 @@
 
    [:div {:class "patient-container"}
     [table-header {:buttons [^{:key :create-patient}
-                             [ui/button
+                             [button
                               {:id :create-patient
-                               :label :app/add-patient}]]
+                               :label :app/add-patient
+                               :on-click #(rf/dispatch [::nav/set-active-page :patient])}]]
                    :id :table}]
 
     [table {:id :table
             :sub :patients
             :sorted-by :fullname
-            :on-click-row (fn [])
+            :on-click-row (fn [patient] (rf/dispatch [::nav/set-active-page :patient (:identifier patient)]))
             :fields [{:title :patient/fullname
                       :value :fullname
                       :filter-type :input}
