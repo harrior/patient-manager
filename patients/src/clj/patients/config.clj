ac5bad857e34de0d6a20748cef8eaab301be8f34
Sergey Sizov <harrior@gmail.com>
2023-03-07 17:48:22 +0400

    feat: added params for testing db

--- patients/src/clj/patients/config.clj
+++ patients/src/clj/patients/config.clj
@@ -15,8 +15,26 @@
    :password "postgres"})
 
 (def
+  ^{:doc "Configuration for Database (test)."}
+  db-spec-test
+  "A map containing the database connection details"
+  {:dbtype "postgresql"
+   :dbname "medicine_test"
+   :host "localhost"
+   :user "postgres"
+   :password "postgres"})
+
+
+(def
   ^{:doc "Configuraton for Migratus."}
   migratus-config
   {:store :database
    :migration-dir "migrations"
    :db db-spec})
+
+(def
+  ^{:doc "Configuraton for Migratus (test)."}
+  migratus-config-test
+  {:store :database
+   :migration-dir "migrations"
+   :db db-spec-test})
--- patients/src/clj/patients/db.clj
+++ patients/src/clj/patients/db.clj
@@ -1,5 +1,5 @@
 (ns patients.db
-  "Namespace for connecting to the patients database and performing migrations."
+  "Connects to the patients database and performs migrations."
   (:require [clojure.tools.logging :as log]
             [migratus.core :as migratus]
             [next.jdbc :as jdbc]
@@ -7,8 +7,14 @@
             [patients.db-jsonb]))
 
 (log/info "Connect to Database")
-(def db-conn (jdbc/get-datasource config/db-spec))
+
+(def db-conn
+  "The database connection"
+  (jdbc/get-datasource config/db-spec))
+
 (log/info "Connected to Database.")
 
+;; Initializes the Migratus library for performing database migrations.
 (migratus/init config/migratus-config)
+;; Runs database migrations using the Migratus library.
 (migratus/migrate config/migratus-config)
