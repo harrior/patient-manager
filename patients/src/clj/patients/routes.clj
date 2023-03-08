a021cf039d2ba32e96f161a2cd07e7b6d3734e16
Sergey Sizov <harrior@gmail.com>
2023-03-09 01:24:30 +0400

    feat: added wrap-stacktrace for router

--- patients/src/clj/patients/routes.clj
+++ patients/src/clj/patients/routes.clj
@@ -3,6 +3,7 @@
   (:require [compojure.core :refer [GET POST defroutes]]
             [compojure.route :refer [not-found resources]]
             [ring.middleware.edn :as edn]
+            [ring.middleware.stacktrace :refer [wrap-stacktrace]]
             [ring.util.response :refer [redirect]]
             [patients.rpc :as rpc]))
 
@@ -16,4 +17,5 @@
 (def app
   "Defines the top-level middleware stack."
   (-> handler
+      (wrap-stacktrace)
       edn/wrap-edn-params))
