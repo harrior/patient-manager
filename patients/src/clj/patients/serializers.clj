36b04da286e4a041ab794bdb614e6cdf778a89d4
Sergey Sizov <harrior@gmail.com>
2023-03-09 02:03:55 +0400

    refactor: refactored serializer, added test and docs

--- patients/src/clj/patients/serializers.clj
+++ patients/src/clj/patients/serializers.clj
@@ -1,22 +1,32 @@
-(ns patients.serializers)
+(ns patients.serializers
+  "Namespace for serialising patient data.")
 
 (defn patient-serialiser
   "Serialises patient data.
-   Accepts a `patient` record, and returns a serialised flat map of patient data"
+   Accepts a `patient` record, and returns a serialised flat map of patient data
+
+  Args:
+    patient (map): A patient record.
+
+  Returns:
+    A map containing the serialised patient data with the following keys:
+      - :insurance-number: The patient's insurance number.
+      - :fullname: The patient's full name.
+      - :address: The patient's address.
+      - :gender: The patient's gender.
+      - :birth-date: The patient's birth date.
+      - :identifier: The patient's identifier.
+
+  Raises:
+    Exception: If any required fields are missing in the `patient` record."
   [patient]
   (let [patient* (:patients/patient patient)
-        insurance-number (:insurance-number patient*)
-        fullname (-> patient*
-                     :name
-                     first
-                     :text)
-        address (-> patient*
-                    :address
-                    first
-                    :text)
+        fullname (get-in patient* [:name 0 :text])
+        address (get-in patient* [:address 0 :text])
         gender (:gender patient*)
         birth-date (:birth-date patient*)
-        identifier (:patients/id patient)]
+        identifier (:patients/id patient)
+        insurance-number (:insurance-number patient*)]
     {:insurance-number insurance-number
      :fullname fullname
      :address address
---
+++ patients/test/clj/patients/serializers_test.clj
@@ -0,0 +1,29 @@
+(ns patients.serializers-test
+  (:require [clojure.test :refer :all]
+            [patients.serializers :as serializers]))
+
+(deftest patient-serialiser-test
+    (let [patient {:patients/id #uuid "65c09345-28c9-4868-90b7-de56cec20b2a"
+                   :patients/patient {:name
+                                      [{:use "usual"
+                                        :text "Петров Иван Иванович" :family "Петров"
+                                        :given ["Иван" "Иванович"]}]
+                                      :address [{:use "home"
+                                                 :city "г. Москва"
+                                                 :type "physical"
+                                                 :state "Московская область"
+                                                 :line "ул. Ленина, д. 12"
+                                                 :postalCode "123456"
+                                                 :country "RU"
+                                                 :district "Басманный"
+                                                 :text "123456, Московская область, г. Москва, ул. Ленина, д. 12"}]
+                                      :insurance-number "9876543210123456"
+                                      :gender "male"
+                                      :birth-date "1979-01-01"}}]
+      (is (= (serializers/patient-serialiser patient)
+             {:insurance-number "9876543210123456",
+              :fullname "Петров Иван Иванович",
+              :address "123456, Московская область, г. Москва, ул. Ленина, д. 12",
+              :gender "male",
+              :birth-date "1979-01-01",
+              :identifier #uuid "65c09345-28c9-4868-90b7-de56cec20b2a"}))))
