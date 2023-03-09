0ea80b486acd4bae2071a7bcaf1c350131ba8b42
Sergey Sizov <harrior@gmail.com>
2023-03-09 18:06:36 +0400

    refactor: refactor grid page

--- patients/src/cljs/patients/pages/patient_grid.cljs
+++ patients/src/cljs/patients/pages/patient_grid.cljs
@@ -2,9 +2,9 @@
   (:require [re-frame.core :as rf]
             [patients.nav :as nav]
             [patients.components.locale :refer [locale]]
-            [patients.components.table :refer [table table-header]]
+            [patients.components.table :as table]
             [patients.components.requests :as rpc]
-            [patients.components.ui-elements :refer [button show-error-popup]]))
+            [patients.components.ui-elements :as ui]))
 
 ;;
 ;; Subs
@@ -30,62 +30,65 @@
                reverse))))
 
 (rf/reg-event-fx
- :bad-http-result
- (fn [_ _]
-   (show-error-popup :app/bad-request)
-   {}))
-
-(rf/reg-event-fx
  :request-patients-list
  (fn [_ _]
    {:dispatch [::rpc/invoke
                {:method :list-patients}
                [:load-patients-db]
-               [:bad-http-result]]}))
+               [::ui/show-error-popup :app/bad-request]]}))
 
 ;;
 ;; Patients grid page
 ;;
 
-(defn init []
+(defn- init []
   (rf/dispatch [:request-patients-list]))
 
-(defn patient-grid-page
+(defn- page-header
   []
-  [:div {:class "container"}
-   [:header {:class "header"}
-    [:h1 (locale :app/title)]]
+  [:header {:class "header"}
+   [:h1 (locale :app/title)]])
 
-   [:div {:class "patient-container"}
-    [table-header {:buttons [^{:key :create-patient}
-                             [button
-                              {:id :create-patient
-                               :label :app/add-patient
-                               :on-click #(rf/dispatch [::nav/set-active-page :patient])}]]
-                   :id :table}]
+(defn- table-header
+  []
+  [table/table-header
+   {:buttons [^{:key :create-patient}
+              [ui/button
+               {:id :create-patient
+                :label :app/add-patient
+                :on-click #(rf/dispatch [::nav/set-active-page :patient])}]]
+    :id :table}])
 
-    [table {:id :table
-            :sub :patients
-            :sorted-by :fullname
-            :on-click-row (fn [patient] (rf/dispatch [::nav/set-active-page :patient (:identifier patient)]))
-            :fields [{:title :patient/fullname
-                      :value :fullname
-                      :filter-type :input}
-                     {:title :patient/gender
-                      :value :gender
-                      :filter-type :select}
-                     {:title :patient/birthday
-                      :value :birth-date
-                      :filter-type :date}
-                     {:title :address/text
-                      :value :address
-                      :filter-type :input}
-                     {:title :patient/insurance-number
-                      :value :insurance-number
-                      :filter-type :input}]}]]
-   [:footer {:class "footer"}]])
+(defn- table
+  []
+  [:div {:class "patient-container"}
+   [table/table
+    {:id :table
+     :sub :patients
+     :sorted-by :fullname
+     :on-click-row (fn [patient]
+                     (rf/dispatch [::nav/set-active-page :patient (:identifier patient)]))
+     :fields [{:title :patient/fullname
+               :value :fullname
+               :filter-type :input}
+              {:title :patient/gender
+               :value :gender
+               :filter-type :select}
+              {:title :patient/birthday
+               :value :birth-date
+               :filter-type :date}
+              {:title :address/text
+               :value :address
+               :filter-type :input}
+              {:title :patient/insurance-number
+               :value :insurance-number
+               :filter-type :input}]}]])
 
 (defn main
   []
   (init)
-  [patient-grid-page])
+  [:div.container
+   [page-header]
+   [table-header]
+   [table]
+   [ui/footer]])
